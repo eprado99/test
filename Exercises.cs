0cbf238d12bea36b793d912384936e6f5db62b74
eprado99 <a01570318@itesm.mx>
2023-05-28 13:01:31 -0600

    first commit

---
+++ Exercises.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoCLI
+{
+    public static class Exercises
+    {
+        public static string ReverseString(string str)
+        {
+            if(str == "")
+            {
+                return "";
+            }
+            var charArr = str.ToCharArray();
+            var leftIdx = 0;
+            var rightIdx = str.Length - 1;
+            while(leftIdx < rightIdx)
+            {
+                char temp = charArr[leftIdx];
+                charArr[leftIdx] = charArr[rightIdx];
+                charArr[rightIdx] = temp;
+                leftIdx++;
+                rightIdx--;
+            }
+            return new string(charArr);
+        }
+
+        public static void IsEvenOrOdd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"El numero {num} es par");
+            }
+            else
+            {
+                Console.WriteLine($"El numero {num} es impar");
+            }
+        }
+
+        public static bool PrintColorBasedTableWithIf(string color)
+        {
+            /*
+             * Amarillo = La Tabla del 2. 
+             * Morado = Tabla del 7
+             * Rosa = Tabla del 9
+             * Azul = Tabla del 6
+             * Blanco = Tabla del 12
+            */
+            if(color == "Amarillo")
+            {
+                PrintMultiplicationTable(2);
+            } else if(color == "Morado")
+            {
+                PrintMultiplicationTable(7);
+            } else if(color == "Rosa")
+            {
+                PrintMultiplicationTable(9);
+            } else if(color == "Azul")
+            {
+                PrintMultiplicationTable(6);
+            } else if(color == "Blanco")
+            {
+                PrintMultiplicationTable(12);
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool PrintColorBasedTableWithSwitch(string color)
+        {
+            switch (color)
+            {
+                case "Amarillo":
+                    PrintMultiplicationTable(2);
+                    break;
+                case "Morado":
+                    PrintMultiplicationTable(7);
+                    break;
+                case "Rosa":
+                    PrintMultiplicationTable(9);
+                    break;
+                case "Azul":
+                    PrintMultiplicationTable(6);
+                    break;
+                case "Blanco":
+                    PrintMultiplicationTable(12);
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+        
+        public static void PrintMultiplicationTable(int n)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n * i}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using static DecoCLI.Exercises;
+
+bool exit = false;
+while (!exit)
+{
+    Console.WriteLine("Seleccione una opcion");
+    Console.WriteLine("1. Invertir un string");
+    Console.WriteLine("2. Revisar si un numero es par o impar");
+    Console.WriteLine("3. Imprimir tabla de multiplicacion dependiendo del color (if else)");
+    Console.WriteLine("4. Imprimir tabla de multiplicacion dependiendo del color (switch)");
+    Console.WriteLine("5. Salir");
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            Console.WriteLine("Ingresa un string");
+            string str = Console.ReadLine();
+            str = ReverseString(str);
+            Console.WriteLine(str);
+            break;
+        case "2":
+            Console.WriteLine("Ingresa un numero");
+            bool isNum = false;
+            while (!isNum)
+            {
+                isNum = int.TryParse(Console.ReadLine(), out int n);
+                if (!isNum)
+                {
+                    Console.WriteLine("Ingresa un numero");
+                }
+                else
+                {
+                    IsEvenOrOdd(n);
+                }
+            }
+            break;
+            
+        case "3":
+            Console.WriteLine("Ingresa un color");
+            bool validColor = false;
+            while (!validColor)
+            {
+                var res = PrintColorBasedTableWithIf(Console.ReadLine());
+                validColor = res;
+                if (!validColor)
+                {
+                    Console.WriteLine("Ingresa un color valido");
+                    Console.WriteLine("* Amarillo = La Tabla del 2. \r\n* Morado = Tabla del 7\r\n* Rosa = Tabla del 9\r\n* Azul = Tabla del 6\r\n* Blanco = Tabla del 12");
+                }
+            }
+            break;
+        case "4":
+            Console.WriteLine("Ingresa un color");
+            bool validColorS = false;
+            while (!validColorS)
+            {
+                var res = PrintColorBasedTableWithSwitch(Console.ReadLine());
+                validColorS = res;
+                if (!validColorS)
+                {
+                    Console.WriteLine("Ingresa un color valido");
+                }
+            }
+            break;
+        case "5":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Opcion no valida");
+            break;
+    }
+}
+
